445feae0ce09bf682dd15b662a2f60334ade3a55
Kananshm <53233067+Kananshm@users.noreply.github.com>
2019-11-05 14:11:35 -0800

    homework

---
+++ StringMethods_05_11_2019/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMethods_05_11_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringMethod Str = new StringMethod();
+            Str.str = "Musa Musayev";
+
+
+            string mes = "Kanan Mammmadli";
+
+            Console.WriteLine(Str.str.Replace("ayev","Altay"));
+            //string mes = "Kanan Mammadli";
+
+            //Console.WriteLine(mes.Length); 
+
+            //Str.IndexOf("s");
+            //.WriteLine(Str.IndexOf("v"));
+            //Str.GetLength();
+            //Console.WriteLine(Str.GetLength());
+
+
+            //Str.Substring(1, 4);
+            //Console.WriteLine(Str.Substring(1,4));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringMethods_05_11_2019/StringMethod.cs
@@ -0,0 +1,69 @@
+﻿namespace StringMethods_05_11_2019
+{
+    class StringMethod
+    {
+        public string str;
+        
+
+        public int GetLength(string phrase)
+        {
+            int length = 0;
+            foreach (char i in str)
+            {
+                length++;
+            }
+            return length;
+        }
+
+        public string Substring(int fromIndex, int count)
+        {
+            string result = string.Empty;
+            for (int i = fromIndex; i < fromIndex + count; i++)
+            {
+                result += str[i]; 
+            }
+            return result;
+        }
+
+        public int IndexOf(string phrase)
+          {
+            int indx = -1;
+            int phraseLength = phrase.Length;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Substring(i, GetLength(phrase)) == phrase)
+                {
+                    indx = i;
+                    
+                }
+            }
+            return indx;
+        }
+        public bool Contains(string phrase)
+        {
+            bool indx = false;
+            int phraseLength = phrase.Length;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Substring(i, GetLength(phrase)) == phrase)
+                {
+                    indx = true;
+
+                }
+            }
+            return indx;
+        }
+        public string Replace(string oldValue,string newValue)
+        {
+            int oldval = IndexOf(oldValue);
+            int from = oldval + oldValue.Length;
+            string old = Substring(0, oldval);
+            string rightSide = Substring( from, str.Length - from);
+
+            return old + newValue + rightSide;
+        }
+
+    }
+}
